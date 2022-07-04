7604a36602d70ea85141a553ebe46e715914c341
Mesquita <mickaelmendes50@gmail.com>
2022-07-04 09:32:45 -0300

    Fix pd formation issue

--- bisseccao_newton.py
+++ bisseccao_newton.py
@@ -1,4 +1,11 @@
-import math #biblioteca de matemática em pyhton
+import math
+from this import d #biblioteca de matemática em pyhton
+import pandas as pd #modulo para análise de dados
+
+#listas
+lx = []
+lf = []
+f_linha = []
 
 #definir uma função
 def f(x): 
@@ -13,6 +20,8 @@ def m_biseccao(a,b,e):
     # intervalo [a,b]
     # erro e
     #Teorema de Bolzano
+    print("\nMétodo de Bisseção\n")
+
     #-Verificar se existe raiz nesse intervalo
     if(f(a)*f(b)) < 0:
         #enquanto o módulo de b-a / 2 for maior do que o erro, executa o laço 
@@ -27,16 +36,22 @@ def m_biseccao(a,b,e):
                 else:
                     a = xi
         print("Raiz = ", xi)
+        
     else:
         print("Não existe raiz neste intervalo!")
-
+    
 def m_newton(x0, err, n0):
     # x0 = aproximação inicial
     # err = erro 
     # n0 = número de iterações
+    print("\nMétodo de Newton\n")
 
-    i = 1;
+    i = 0;
     while(math.fabs(f(x0))>err):
+        lx.append(x0)
+        lf.append(f(x0))
+        f_linha.append(df(x0))
+
         x = x0 - f(x0)/df(x0)
         x0 = x
         i = i + 1
@@ -46,12 +61,17 @@ def m_newton(x0, err, n0):
 
     if(i<n0):
         print("Raiz %f\nIterações: %f\nf(%f) = %f\n\n"%(x0,i,x0,f(x0)))
+        dataF = pd.DataFrame(list(zip(lx,lf,f_linha)), columns = ['x', 'f(x)', 'f_linha(x)'])
+        return dataF
 
 #------------------------------------------
 #Execução
 
 #Chamando método da bissecção
-m_biseccao(2,3,0.00001)
+m_biseccao(2,3,0.1)
 
 #Chamando método de newton
-m_newton(1,0.00001,100)
+dataFN = m_newton(1,0.00001,100)
+
+print(dataFN)
+#dataF = pd.DataFrame(list(zip(lx,lf,f_linha)), columns = ['x', 'f(x)', 'f_linha(x)'])
